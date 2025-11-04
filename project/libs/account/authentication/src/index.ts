ed2705b8f4fe24864e14170dc901ac7b184c6aae
yura-posledov <yura-posledov@example.com>
2025-11-04 17:04:55 +0300

    2.10 Добавление интерфейса StorableEntity

--- project/libs/account/authentication/src/index.ts
+++ project/libs/account/authentication/src/index.ts
@@ -1,3 +1,1 @@
 export { AuthenticationModule } from './authentication-module/authentication.module';
-export { AuthenticationController } from './authentication-module/authentication.controller';
-export { AuthenticationService } from './authentication-module/authentication.service';
