796a480815264f40a5f9385e4788890da36aaf0b
yura-posledov <yura-posledov@example.com>
2025-11-08 09:49:11 +0300

    2.27 Добавит аннотации для OpenAPI

--- project/libs/account/authentication/src/rdo/logged-user.rdo.ts
+++ project/libs/account/authentication/src/rdo/logged-user.rdo.ts
@@ -1,12 +1,25 @@
 import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoggedUserRdo {
+  @ApiProperty({
+    description: 'The uniq user ID',
+    example: '134ce8babd-cc30-4805-9b12-d9420398e7c5',
+  })
   @Expose()
   public id: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@user.local'
+  })
   @Expose()
   public email: string;
 
+  @ApiProperty({
+    description: 'Access token',
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ'
+  })
   @Expose()
   public accessToken: string;
 }
