373cc2562c8835e8b4f6bbf448dc976cb5c03414
yura-posledov <yura-posledov@example.com>
2025-12-06 08:20:37 +0300

    3.9. Подключит AccountConfigModule в account.

--- project/apps/account-service/src/app/app.module.ts
+++ project/apps/account-service/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationModule } from '@project/authentication';
 import { BlogUserModule } from '@project/blog-user';
+import { AccountConfigModule } from '@project/account-config';
 
 @Module({
-  imports: [AuthenticationModule, BlogUserModule],
+  imports: [AuthenticationModule, BlogUserModule, AccountConfigModule],
   controllers: [],
   providers: [],
 })
