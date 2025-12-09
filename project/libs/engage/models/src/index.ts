13507cf32fd57ba16b3735371b2d6b2e019cde69
yura-posledov <yura-posledov@example.com>
2025-12-09 15:51:04 +0300

    4.17-4.18. Добавит модуль PrismaClientModule и базовый репозиторий для работы с PostgreSQL.

--- project/libs/engage/models/src/index.ts
+++ project/libs/engage/models/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/models';
+
+export { PrismaClientModule } from './prisma-client-module/prisma-client.module';
+export { PrismaClientService } from './prisma-client-module/prisma-client.service';
