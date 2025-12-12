2a0b1375ab75ff89731be1f1ee852202d457670e
yura-posledov <yura-posledov@example.com>
2025-12-12 15:11:20 +0300

    5.5. Активирует трансформацию для ValidationPipe

--- project/apps/account-service/src/main.ts
+++ project/apps/account-service/src/main.ts
@@ -23,7 +23,9 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
 
   const configService = app.get(ConfigService);
   const port = configService.get('application.port');
