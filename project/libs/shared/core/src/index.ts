5a5ae928f359b876efe444186ca2ae7e90e8e740
yura-posledov <yura-posledov@example.com>
2025-12-13 04:40:08 +0300

    5.18. Добавит интерфейсы Token и TokenPayload

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -17,3 +17,5 @@ export { StorableEntity } from './lib/interfaces/storable-entity.interface';
 export { EntityFactory } from './lib/interfaces/entity-factory.interface';
 export { PaginationResult } from './lib/interfaces/pagination.interface';
 export { SortDirection } from './lib/interfaces/sort-direction.interface';
+export { Token } from './lib/interfaces/token.interface';
+export { TokenPayload } from './lib/interfaces/token-payload.interface';
