c9fb25abce1b8b741e9517fa5a4c46cc11442c67
yura-posledov <yura-posledov@example.com>
2025-12-14 10:14:58 +0300

    6.25. Добавит EmailSubscriberController.

--- project/libs/notify/email-subscriber-api/src/email-subscriber-api-module/email-subscriber-api.controller.ts
+++ project/libs/notify/email-subscriber-api/src/email-subscriber-api-module/email-subscriber-api.controller.ts
@@ -1,24 +1,23 @@
-import { Controller, Logger } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
+
 import { RabbitRouting } from '@project/core';
 import { EmailSubscriberApiService } from './email-subscriber-api.service';
 import { CreateSubscriberDto } from '../dto/create-subscriber.dto';
 
+
 @Controller()
 export class EmailSubscriberApiController {
-  private readonly logger = new Logger(EmailSubscriberApiController.name);
-
-  constructor(private readonly emailSubscriberApiService: EmailSubscriberApiService) {}
+  constructor(
+    private readonly subscriberService: EmailSubscriberApiService,
+  ) {}
 
   @RabbitSubscribe({
-    exchange: 'typoteka.notify',
+    exchange: 'readme.notify.income',
     routingKey: RabbitRouting.AddSubscriber,
-    queue: 'typoteka.notify.income',
+    queue: 'readme.notify.income',
   })
-  public async create(subscriber: CreateSubscriberDto): Promise<void> {
-    this.logger.log(`Received subscriber: ${JSON.stringify(subscriber)}`);
-    await this.emailSubscriberApiService.addSubscriber(subscriber);
-    this.logger.log(`Subscriber ${subscriber.email} added successfully`);
+  public async create(subscriber: CreateSubscriberDto) {
+    this.subscriberService.addSubscriber(subscriber);
   }
 }
-
--- project/libs/notify/email-subscriber-api/src/email-subscriber-api-module/email-subscriber-api.module.ts
+++ project/libs/notify/email-subscriber-api/src/email-subscriber-api-module/email-subscriber-api.module.ts
@@ -1,37 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
-import { ConfigService } from '@nestjs/config';
 import { EmailSubscriberModule } from '@project/email-subscriber';
 import { EmailSubscriberApiController } from './email-subscriber-api.controller';
 import { EmailSubscriberApiService } from './email-subscriber-api.service';
-
-function getRabbitMQOptions(configService: ConfigService) {
-  const user = configService.get<string>('rabbit.user');
-  const password = configService.get<string>('rabbit.password');
-  const host = configService.get<string>('rabbit.host');
-  const port = configService.get<number>('rabbit.port');
-  const exchange = configService.get<string>('rabbit.exchange');
-
-  return {
-    exchanges: [
-      {
-        name: exchange,
-        type: 'direct',
-      },
-    ],
-    uri: `amqp://${user}:${password}@${host}:${port}`,
-    connectionInitOptions: { wait: true },
-    enableControllerDiscovery: true,
-  };
-}
+import { getRabbitMQOptions } from '@project/helpers';
 
 @Module({
   imports: [
     EmailSubscriberModule,
-    RabbitMQModule.forRootAsync({
-      useFactory: getRabbitMQOptions,
-      inject: [ConfigService],
-    }),
+    RabbitMQModule.forRootAsync(getRabbitMQOptions('rabbit')),
   ],
   controllers: [EmailSubscriberApiController],
   providers: [EmailSubscriberApiService],
