839a7437e04772145fc7e0640044f103b8957d47
yura-posledov <yura-posledov@example.com>
2025-12-23 12:06:34 +0300

    7.23. Добавит пример проксирования запросов login и refreshToken.

--- project/libs/account/authentication/src/index.ts
+++ project/libs/account/authentication/src/index.ts
@@ -1,3 +1,5 @@
 export { AuthenticationModule } from './authentication-module/authentication.module';
 export { LoggedUserRdo } from './rdo/logged-user.rdo';
 export { UserRdo } from './rdo/user.rdo';
+export { LoginUserDto } from './dto/login-user.dto';
+export { CreateUserDto } from './dto/create-user.dto';
